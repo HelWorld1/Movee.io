efd3187769065c3d2cb1781d31bdd184cbf099a2
Dmitry Grigorev <helworldd@yandex.ru>
2022-01-21 20:51:10 +0300

    Change blog 'Map', add footer and script for mobile-slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
 	            center: coordinates
 	        });
 	}
-	let Slide = 1;
-	$('.menu').click(function(){
+	$('.menu__btn').click(function(){
 		$('.nav-panel__menu-body').toggleClass('hide_menu');
 	});
 	$('.autopark__arrow-left').click(function (){
@@ -70,6 +69,142 @@ $(document).ready(function(){
 		// 5
 		if($('.autopark__item-1').hasClass('hide_item') && $('.autopark__item-2').hasClass('hide_item') && $('.autopark__item-3').hasClass('hide_item') && $('.autopark__item-4').hasClass('hide_item')){
 			$('.autopark__item-1').toggleClass('hide_item');
+			$('.autopark__item-5').toggleClass('hide_item');
+			return;
+		}
+	});
+	/* mobile slider */
+	/* 1 */
+	$('.autopark__circle-1').click(function (){
+		/* 2 */
+		if(!$('.autopark__item-2').hasClass('hide_item')){
+			$('.autopark__item-2').toggleClass('hide_item');
+			$('.autopark__item-1').toggleClass('hide_item');
+			return;
+		}
+		/* 3 */
+		if(!$('.autopark__item-3').hasClass('hide_item')){
+			$('.autopark__item-3').toggleClass('hide_item');
+			$('.autopark__item-1').toggleClass('hide_item');
+			return;
+		}
+		/* 4 */
+		if(!$('.autopark__item-4').hasClass('hide_item')){
+			$('.autopark__item-4').toggleClass('hide_item');
+			$('.autopark__item-1').toggleClass('hide_item');
+			return;
+		}
+		/* 5 */
+		if(!$('.autopark__item-5').hasClass('hide_item')){
+			$('.autopark__item-5').toggleClass('hide_item');
+			$('.autopark__item-1').toggleClass('hide_item');
+			return;
+		}
+	});
+	/* 2 */
+	$('.autopark__circle-2').click(function (){
+		/* 1 */
+		if(!$('.autopark__item-1').hasClass('hide_item')){
+			$('.autopark__item-1').toggleClass('hide_item');
+			$('.autopark__item-2').toggleClass('hide_item');
+			return;
+		}
+		/* 3 */
+		if(!$('.autopark__item-3').hasClass('hide_item')){
+			$('.autopark__item-3').toggleClass('hide_item');
+			$('.autopark__item-2').toggleClass('hide_item');
+			return;
+		}
+		/* 4 */
+		if(!$('.autopark__item-4').hasClass('hide_item')){
+			$('.autopark__item-4').toggleClass('hide_item');
+			$('.autopark__item-2').toggleClass('hide_item');
+			return;
+		}
+		/* 5 */
+		if(!$('.autopark__item-5').hasClass('hide_item')){
+			$('.autopark__item-5').toggleClass('hide_item');
+			$('.autopark__item-2').toggleClass('hide_item');
+			return;
+		}
+	});
+	/* 3 */
+	$('.autopark__circle-3').click(function (){
+		/* 1 */
+		if(!$('.autopark__item-1').hasClass('hide_item')){
+			$('.autopark__item-1').toggleClass('hide_item');
+			$('.autopark__item-3').toggleClass('hide_item');
+			return;
+		}
+		/* 2 */
+		if(!$('.autopark__item-2').hasClass('hide_item')){
+			$('.autopark__item-2').toggleClass('hide_item');
+			$('.autopark__item-3').toggleClass('hide_item');
+			return;
+		}
+		/* 4 */
+		if(!$('.autopark__item-4').hasClass('hide_item')){
+			$('.autopark__item-4').toggleClass('hide_item');
+			$('.autopark__item-3').toggleClass('hide_item');
+			return;
+		}
+		/* 5 */
+		if(!$('.autopark__item-5').hasClass('hide_item')){
+			$('.autopark__item-5').toggleClass('hide_item');
+			$('.autopark__item-3').toggleClass('hide_item');
+			return;
+		}
+	});
+	/* 4 */
+	$('.autopark__circle-4').click(function (){
+		/* 1 */
+		if(!$('.autopark__item-1').hasClass('hide_item')){
+			$('.autopark__item-1').toggleClass('hide_item');
+			$('.autopark__item-4').toggleClass('hide_item');
+			return;
+		}
+		/* 2 */
+		if(!$('.autopark__item-2').hasClass('hide_item')){
+			$('.autopark__item-2').toggleClass('hide_item');
+			$('.autopark__item-4').toggleClass('hide_item');
+			return;
+		}
+		/* 3 */
+		if(!$('.autopark__item-3').hasClass('hide_item')){
+			$('.autopark__item-3').toggleClass('hide_item');
+			$('.autopark__item-4').toggleClass('hide_item');
+			return;
+		}
+		/* 5 */
+		if(!$('.autopark__item-5').hasClass('hide_item')){
+			$('.autopark__item-5').toggleClass('hide_item');
+			$('.autopark__item-4').toggleClass('hide_item');
+			return;
+		}
+	});
+	/* 5 */
+	$('.autopark__circle-5').click(function (){
+		/* 1 */
+		if(!$('.autopark__item-1').hasClass('hide_item')){
+			$('.autopark__item-1').toggleClass('hide_item');
+			$('.autopark__item-5').toggleClass('hide_item');
+			return;
+		}
+		/* 2 */
+		if(!$('.autopark__item-2').hasClass('hide_item')){
+			$('.autopark__item-2').toggleClass('hide_item');
+			$('.autopark__item-5').toggleClass('hide_item');
+			return;
+		}
+		/* 3 */
+		if(!$('.autopark__item-3').hasClass('hide_item')){
+			$('.autopark__item-3').toggleClass('hide_item');
+			$('.autopark__item-5').toggleClass('hide_item');
+			return;
+		}
+		/* 4 */
+		if(!$('.autopark__item-4').hasClass('hide_item')){
+			$('.autopark__item-4').toggleClass('hide_item');
 			$('.autopark__item-5').toggleClass('hide_item');
 			return;
 		}
